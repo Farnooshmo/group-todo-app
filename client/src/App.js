e6c21927170406989db4421b6f7c41fe7110f66b
Farnoosh <91014204+Farnooshmo@users.noreply.github.com>
2023-05-28 10:59:49 +0100

    Update ListItem component

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,36 @@
-import ListHeader from './components/ListHeader'
+import ListHeader from "./components/ListHeader";
 import { useEffect, useState } from "react";
 import ListItem from "./components/ListItem";
 
-
-
 const App = () => {
-const userEmail = "farnooshmoayeri@gmail.com";
-const [tasks,setTasks] = useState(null)
+  const userEmail = "farnooshmoayeri@gmail.com";
+  const [tasks, setTasks] = useState(null);
 
-const getData =async() =>{
-  try {
-    const response = await fetch(`http://localhost:9090/todos/${userEmail}`)
-    const json = response.json()
-    setTasks(json)
-  } catch (err) { 
-    console.error(err)
-  }
-}
+  const getData = async () => {
+    try {
+      const response = await fetch(`http://localhost:9090/todos/${userEmail}`);
+      const json = response.json();
+      setTasks(json);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
-//Sort by date
-const sortedTasks = tasks?.sort((a,b)=> new Date(a.date) - new Date(b.date))
+  //Sort by date
+  const sortedTasks = tasks?.sort(
+    (a, b) => new Date(a.date) - new Date(b.date)
+  );
 
-useEffect(()=>getData,[])
+  useEffect(() => getData, []);
 
- return (
-   <div className="app">
-    <ListHeader listName={"🏝️ Holiday tick list"} />
-    {sortedTasks?.map((task) => <ListItem key={task.id} task={task} />)}
-   </div>
- );
-}
+  return (
+    <div className="app">
+      <ListHeader listName={"🏝️ Holiday tick list"} />
+      {sortedTasks?.map((task) => (
+        <ListItem key={task.id} task={task} />
+      ))}
+    </div>
+  );
+};
 
 export default App;
